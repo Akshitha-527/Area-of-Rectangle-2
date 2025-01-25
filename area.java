fc662e799588a1707fea99de91f790df103ef295
Akshitha-527 <nampallyakshitha5@gmail.com>
2025-01-25 14:35:42 +0530

    Update area.java

--- area.java
+++ area.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class area {
+public class Area {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter length: ");
-        float breadth=sc.nextFloat();
+        float breadth = sc.nextFloat();
         System.out.println("Enter Width: ");
-        float length=sc.nextFloat();
+        float length = sc.nextFloat();
         sc.close();
         System.out.println("Area of Rectangle:"+(breadth*length));
        
